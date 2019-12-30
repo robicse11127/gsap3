33aac45b86815656531479cc6ffe77b5645f1a29
rabiul <rabiul@bitmascot.com>
2019-12-30 17:31:56 +0600

    advance-staggers

--- advance-staggers/scripts.js
+++ advance-staggers/scripts.js
@@ -1,5 +1,15 @@
-var tl = gsap.timeline();
+var tl = new TimelineMax({repeat: -1, repeatDelay: 0.5});
 
-tl.from('.astronaut', {duration: 2, x: '50%', ease: 'back', rotation: 360})
-tl.to( '.astronaut', { duration: 2, rotation: 360, ease: 'back' } )
-tl.from( '.circle', {duration: 1, opacity: 0, y: () => Math.random() * 400 - 200, stagger: 0.25}, '-=1' )
+tl.staggerTo('.sqr', 1, {
+    scale: 0.1,
+    y: 50,
+    repeat: 1,
+    yoyo: true,
+    stagger: {
+        // each: 0.1,
+        amount: 2, // positive or negative
+        from: 'center', // end, start, center, index
+        ease: Power1.easeOut, // easing effect,
+        axis: 'x'
+    }
+})
